521c9b157c58401f7087e58807e4e3e277472be3
chickenidol <chickenidol@gmail.com>
2023-09-25 10:42:42 +0300

    first commit

---
+++ quote2dollar.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+from lib.core.compat import xrange
+from lib.core.enums import PRIORITY
+
+__priority__ = PRIORITY.LOW
+
+def dependencies():
+    pass
+
+def tamper(payload, **kwargs):
+    retVal = payload
+
+    if payload:
+        retVal = ""
+
+        for i in xrange(len(payload)):
+            if payload[i] == '\'':
+                retVal += '$$'
+                continue
+
+            retVal += payload[i]
+
+    return retVal
